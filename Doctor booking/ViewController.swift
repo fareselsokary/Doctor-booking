f2c2a96f77c46bbfeb8853f37bc55136bf74c17c
fares elsokary <fareselsokary@faress-mac.local>
2018-10-07 10:12:15 +0200

    Initial Commit

---
+++ Doctor booking/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Doctor booking
+//
+//  Created by fares elsokary on 10/7/18.
+//  Copyright © 2018 FaresElsokary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
